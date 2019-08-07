29b7908347d20b90e685987e0ae57090aa7ba329
Peter Jang <peter.x.jang@gmail.com>
2019-08-06 20:34:48 -0500

    Add Spotify authorize action to obtain access token

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -8,4 +8,18 @@ class Api::PagesController < ApplicationController
     response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ENV["NEWSAPI_KEY"]}")
     render json: response.parse
   end
+
+  def spotify_authorize
+    response = HTTP.post(
+      "https://accounts.spotify.com/api/token",
+      form: {
+        grant_type: "authorization_code",
+        code: params[:code],
+        redirect_uri: "http://localhost:8080",
+        client_id: ENV["SPOTIFY_CLIENT_ID"],
+        client_secret: ENV["SPOTIFY_CLIENT_SECRET"],
+      },
+    )
+    render json: response.parse
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     get "/reddit_programming" => "pages#reddit_programming"
     get "/newsapi_headlines" => "pages#newsapi_headlines"
+    post "/spotify_authorize" => "pages#spotify_authorize"
   end
 end
