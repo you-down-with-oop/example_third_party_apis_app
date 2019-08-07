6710a3605d8b4ab06a7fa93a9f210f07efc43a87
Peter Jang <peter.x.jang@gmail.com>
2019-08-06 19:44:14 -0500

    Add action for NewsAPI web request

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -3,4 +3,9 @@ class Api::PagesController < ApplicationController
     response = HTTP.get("https://www.reddit.com/r/programming/.json")
     render json: response.parse
   end
+
+  def newsapi_headlines
+    response = HTTP.get("https://newsapi.org/v2/top-headlines?country=us&apiKey=#{ENV["NEWSAPI_KEY"]}")
+    render json: response.parse
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/reddit_programming" => "pages#reddit_programming"
+    get "/newsapi_headlines" => "pages#newsapi_headlines"
   end
 end
